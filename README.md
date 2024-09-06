77eb017459b07ba5ca1d9d3ed170aab577ca13ab
yeniurtasi <163404914+yeniurtasi@users.noreply.github.com>
2024-09-06 07:20:51 +0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# ngentotsamapacar
-ngentotsamapacar
+A live demo of the project is available at [https://ngentotsamapacar.pages.dev](https://ngentotsamapacar.pages.dev).
+Fork the [repository](https://github.com/harlahsaduki) on GitHub.
---
+++ app/layout.tsx
@@ -0,0 +1,97 @@
+import "./globals.css";
+import type { Metadata } from "next";
+import { Nunito } from "next/font/google";
+import { SITENAME } from "@/lib/constants";
+import { ThemeProvider } from "@/components/theme-provider";
+
+const font = Nunito({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+    title: `${SITENAME} - Video Bokep Indo Viral Terbaru`,
+    description: `${SITENAME} Video Bokep Indo Jepang Jav Barat Simontok Viral Terbaru Bocil Ngentot Jilbab Smp Mama Sma korea china tante live paksa ngentot abg cewek pijat.`,
+    metadataBase: new URL("https://ngentotsamapacar.pages.dev"),
+    alternates: {
+        canonical: `/`,
+    },
+    openGraph: {
+        title: `${SITENAME} - Video Bokep Indo Jepang Barat Viral Terbaru`,
+        description: `${SITENAME} Video Bokep Indo Jepang Jav Barat Simontok Viral Terbaru Bocil Ngentot Jilbab Smp Mama Sma korea china tante live paksa ngentot abg cewek pijat.`,
+        url: `/`,
+        type: `website`,
+    },
+    verification: {
+        google: 'Q3_1uB12h8T44cCQZkXD2Vqh-V__qfhhInz_4Xa_IiQ',
+        yandex: '2c1f88b02339bf3a',
+    },
+};
+
+export const runtime = "edge";
+
+export default function RootLayout({
+    children,
+}: {
+    children: React.ReactNode;
+}) {
+    const jsonLd = {
+        "@context": "https://schema.org",
+        "@type": "Organization",
+        "name": `${SITENAME}`,
+        "description": `${SITENAME} Video Bokep Indo Jepang Jav Barat Simontok Viral Terbaru Bocil Ngentot Jilbab Smp Mama Sma korea china tante live paksa ngentot abg cewek pijat.`,
+        "logo": "https://ngentotsamapacar.pages.dev/favicon.ico",
+        "url": "https://ngentotsamapacar.pages.dev",
+        "founder":{
+            "@type":"Person",
+            "name":"admin",
+            "url":"https://ngentotsamapacar.pages.dev"
+            },
+            "foundingDate":"2024-08-29"
+        }
+        const jsonLd1 = {
+            "@context": "https://schema.org",
+        "@type": "WebPage",
+        "name": `${SITENAME}`,
+        "url": "https://ngentotsamapacar.pages.dev",
+        "description": `${SITENAME} Video Bokep Indo Jepang Jav Barat Simontok Viral Terbaru Bocil Ngentot Jilbab Smp Mama Sma korea china tante live paksa ngentot abg cewek pijat.`,
+        "image": "https://ngentotsamapacar.pages.dev/favicon.ico",
+        "potentialAction":{
+            "@type":"ReadAction",
+            "target":"https://ngentotsamapacar.pages.dev"}
+        }
+        const jsonLd2 = {
+            "@context": "https://schema.org",
+        "@type": "WebSite",
+        "name": `${SITENAME}`,
+        "url": "https://ngentotsamapacar.pages.dev",
+        "description": `${SITENAME} Video Bokep Indo Jepang Jav Barat Simontok Viral Terbaru Bocil Ngentot Jilbab Smp Mama Sma korea china tante live paksa ngentot abg cewek pijat.`,
+        "potentialAction": { 
+        "@type": "SearchAction", 
+          "target": "https://ngentotsamapacar.pages.dev/?q={search_term}", 
+            "query-input": "required name=search_term"}
+        }
+    return (
+        <html lang="en" suppressHydrationWarning>
+            <body className={font.className}>
+                <ThemeProvider
+                    attribute="class"
+                    defaultTheme="system"
+                    enableSystem
+                    disableTransitionOnChange
+                >
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
+        />
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd1) }}
+        />
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd2) }}
+        />
+                    {children}
+                </ThemeProvider>
+            </body>
+        </html>
+    );
+}
